61118f2536ec43273d72657da9dc68a37a2ef74d
jiaqiluo <jiaqiluo123+GitHub@gmail.com>
2018-02-17 11:14:01 -0800

    commit #4 - assign ramdon id to new user and broadcast to all users

--- index.js
+++ index.js
@@ -8,9 +8,15 @@ app.get('/', function(req,res){
 
 io.on('connection', function(socket){
 	console.log('a user connected');
+
+	socket.on('new user', function(id){
+		io.emit('chat message', 'New User: ' + id);
+	});
+
 	socket.on('disconnect', function(){
 		console.log('user disconnected');
 	});
+
 	socket.on('chat message', function(msg){
 		io.emit('chat message', msg);
 		console.log('message:' + msg);
